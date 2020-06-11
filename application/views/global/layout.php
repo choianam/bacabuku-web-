9799f5a064b449e446842b2f42c6b864a4bf0825
choianam <58158745+choianam@users.noreply.github.com>
2020-06-12 01:44:23 +0700

    perbaikan

--- application/views/global/layout.php
+++ application/views/global/layout.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="" />
     <meta name="author" content="" />
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('assets2/img/logo.png'); ?>">
     <!--[if IE]>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <![endif]-->
